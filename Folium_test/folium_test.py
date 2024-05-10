fdf70af618fcd84d76835b33304e347c217f8e26
Jan Philip Årtun <janphilipartun@MacBook-Air-5.local>
2024-05-10 12:03:23 +0200

    Added marker to Folium

---
+++ Folium_test/folium_test.py
@@ -0,0 +1,43 @@
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+from folium.plugins import Draw
+import folium, io, sys, json
+
+if __name__ == '__main__': 
+    app = QtWidgets.QApplication(sys.argv)
+
+    m = folium.Map(location=[59.092086890317404, 5.910674107222455], zoom_start=19)
+
+    draw = Draw(
+       draw_options={
+          'polyline':False,
+          'rectangle':False,
+          'polygon':False,
+          'circle':False,
+          'marker':True,
+          'circlemarker':False},
+       edit_options={'edit':True})
+    m.add_child(draw)
+
+    data = io.BytesIO()
+    m.save(data, close_file=False)
+
+    class WebEnginePage(QtWebEngineWidgets.QWebEnginePage):
+        def javaScriptConsoleMessage(self, level, msg, line, sourceID):
+            try:
+                geojson = json.loads(msg)
+                if 'geometry' in geojson and 'coordinates' in geojson['geometry']:
+                    coordinates = geojson['geometry']['coordinates']
+                    if isinstance(coordinates[0], list):
+                        # Marker coordinates are nested lists in GeoJSON, so we need to flatten them
+                        coordinates = [item for sublist in coordinates for item in sublist]
+                    coordinates = coordinates[::-1]
+                    print("Marker coordinates:", coordinates)
+            except Exception as e:
+                print("Error parsing JavaScript console message:", e)
+
+    view = QtWebEngineWidgets.QWebEngineView()
+    page = WebEnginePage(view)
+    view.setPage(page)
+    view.setHtml(data.getvalue().decode())
+    view.show()
+    sys.exit(app.exec_())
